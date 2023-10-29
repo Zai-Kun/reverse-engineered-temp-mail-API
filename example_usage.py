2ee73d31393c5713b927c553f1086b185dcc4299
Zai <122824602+Zai-Kun@users.noreply.github.com>
2023-10-29 22:44:52 +0500

    Made it better

--- example_usage.py
+++ example_usage.py
@@ -3,31 +3,51 @@ import asyncio
 from bs4 import BeautifulSoup
 from temp_mail import AsyncTempMail
 
-fetched_emails = []
+fetched_email_ids = []
 
 
-def print_new_email(all_emails):  # function will not work for now, made some changes
+def filter_emails(all_emails):
+    filtered_emails = []
     for email in all_emails:
-        if email[_id] not in fetch_email:
-            print(f"From {email['from']}")
-            print(f"Subject: {email['subject']}")
-            print(f"\nBody: {email['bodyHtml']}")
+        if email["_id"] not in fetched_email_ids:
+            fetched_email_ids.append(email["_id"])
+            filtered_emails.append(email)
+
+    return filtered_emails
+
+
+def print_email(email):
+    print(f"\nFrom: {email.sender}")
+    print(f"Subject: {email.subject}")
+    print(f"Attachments: {email.attachments['count']}")
+    print(f"\nBody: \n{email.text_body}")
+
+    print(email.attachments)
 
 
 async def main():
     async with AsyncTempMail() as temp_mail:
-        response = await temp_mail.fetch_email(token, message_id)
-        html = response["bodyHtml"]
+        response_json = await temp_mail.fetch_new_email_adress()
+        email_adress, token = response_json["mailbox"], response_json["token"]
 
-        # print(temp_mail.html_to_text(html))
-        response_json = await temp_mail.fetch_new_email()
-        email, token = response_json["mailbox"], response_json["token"]
+        print(f"Temp email adress: {email_adress}\nTemp email adress token: {token}\n")
 
-        print(f"Temp email: {email}\nTemp email token: {token}\n")
+        # async for chunch in email.attachments["files"][0].download():
+        #     print(chunch.decode())
 
-        while True:
-            input("Press Enter to check for new emails: ")
-            all_emails = await temp_mail.fetch_all_emails(token)
+        # while True:
+        #     input("Press Enter to check for new emails: ")
+        #     filtered_emails = filter_emails(await temp_mail.fetch_all_emails(token))
+
+        #     if len(filtered_emails) > 0:
+        #         print(f"You have {len(filtered_emails)} new email(s)!")
+        #         for email in filtered_emails:
+        #             complete_email = await temp_mail.fetch_email(
+        #                 token, email["_id"]
+        #             )  # you need to use the 'fetch_email' function to retrieve all details of the email
+        #             print_email(complete_email)
+        #     else:
+        #         print("You do not have any new emails. :(\n")
 
 
 if __name__ == "__main__":
--- temp_mail/async_temp_mail.py
+++ temp_mail/async_temp_mail.py
@@ -4,78 +4,33 @@ from bs4 import BeautifulSoup
 from curl_cffi.requests import AsyncSession
 
 BASE_API_URL = "https://web2.temp-mail.org"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 
 
-class Email:
-    def __init__(self, email_data):
-        self.email_id = email_data["_id"]
-        self.sender = email_data["from"]
-        self.subject = email_data["subject"]
-        self.html_body = email_data["bodyHtml"]
-        self.text_body = self.html_to_text(self.html_body)
-        self.preview = email_data["bodyPreview"]
-        self.attachments = {
-            "count": email_data["attachmentsCount"],
-            "files": email_data["attachments"],
+class Attachment:
+    def __init__(self, attachment, temp_mail_class, token, email_id):
+        self.temp_mail_class = temp_mail_class
+        self.token = token
+        self.email_id = email_id
+
+        self.filename = attachment["filename"]
+        self.attachment_id = attachment["_id"]
+        self.size = attachment["size"]
+        self.mimetype = attachment["mimetype"]
+
+    async def download(self):
+        url = f"{BASE_API_URL}/messages/{self.email_id}/attachment/{self.attachment_id}"
+        headers = {
+            "User-Agent": self.temp_mail_class.user_agent,
+            "Authorization": self.token,
         }
-
-    @staticmethod
-    def html_to_text(html_body):
-        plain_text = html_body.replace("<br>", "\n")
-        soup = BeautifulSoup(plain_text, "html.parser")
-        return soup.get_text()
-
-
-class AsyncTempMail:
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
-
-    def __init__(self, user_agent=None):
-        self.user_agent = self.USER_AGENT if not user_agent else user_agent
-
-    async def __aenter__(self):
-        self.session = AsyncSession(impersonate="chrome110", timeout=99999)
-        return self
-
-    async def __aexit__(self, *args):
-        self.session.close()
-
-    async def fetch_new_email_adress(self):
-        url = f"{BASE_API_URL}/mailbox"
-        headers = {"User-Agent": self.user_agent}
-
-        response = await self.session.post(url, headers=headers)
-        response_json = response.json()
-
-        return response_json
-
-    async def fetch_all_emails(self, token):
-        url = f"{BASE_API_URL}/messages"
-        headers = {"User-Agent": self.user_agent, "Authorization": token}
-
-        response = await self.session.get(url, headers=headers)
-        response_json = response.json()
-
-        return response_json["messages"]
-
-    async def fetch_email(self, token, message_id):
-        url = f"{BASE_API_URL}/messages/{message_id}"
-        headers = {"User-Agent": self.user_agent, "Authorization": token}
-
-        response = await self.session.get(url, headers=headers)
-        response_json = response.json()
-
-        return Email(response_json)
-
-    async def fetch_attachment(self, token, message_id, attachment_id):
-        url = f"{BASE_API_URL}/messages/{message_id}/attachment/{attachment_id}"
-        headers = {"User-Agent": self.user_agent, "Authorization": token}
         response_queue = asyncio.Queue()
 
         async def perform_request():
             def content_callback(chunk):
                 response_queue.put_nowait(chunk)
 
-            response = await self.session.get(
+            response = await self.temp_mail_class.session.get(
                 url=url, headers=headers, content_callback=content_callback
             )
             await response_queue.put(None)
@@ -87,3 +42,89 @@ class AsyncTempMail:
             if chunk is None:
                 break
             yield chunk
+
+    def __repr__(self):
+        return f"Attachment(filename='{self.filename}', attachment_id='{self.attachment_id}', size={self.size}, mimetype='{self.mimetype}')"
+
+
+class DetailedEmail:
+    def __init__(self, detailed_email, temp_mail_class, token):
+        self.email_id = detailed_email["_id"]
+        self.sender = detailed_email["from"]
+        self.subject = detailed_email["subject"]
+        self.html_body = detailed_email["bodyHtml"]
+        self.text_body = self.html_to_text(self.html_body)
+        self.preview = detailed_email["bodyPreview"]
+        self.attachments = {
+            "count": detailed_email["attachmentsCount"],
+            "files": [
+                Attachment(attachment, temp_mail_class, token, self.email_id)
+                for attachment in detailed_email["attachments"]
+            ],
+        }
+
+    @staticmethod
+    def html_to_text(html_body):
+        plain_text = html_body.replace("<br>", "\n")
+        soup = BeautifulSoup(plain_text, "html.parser")
+        return soup.get_text()
+
+    def __repr__(self):
+        return f"DetailedEmail(email_id='{self.email_id}', sender='{self.sender}', subject='{self.subject}', attachments={self.attachments['count']})"
+
+
+class PreviewEmail:
+    def __init__(self, preview_email, temp_mail_class, token):
+        self.temp_mail_class = temp_mail_class
+        self.token = token
+
+        self.email_id = preview_email["_id"]
+        self.sender = preview_email["from"]
+        self.subject = preview_email["subject"]
+        self.preview = preview_email["bodyPreview"]
+        self.attachments = {
+            "count": preview_email["attachmentsCount"],
+        }
+
+    async def fetch_detailed_email(self):
+        url = f"{BASE_API_URL}/messages/{self.email_id}"
+        headers = {
+            "User-Agent": self.temp_mail_class.user_agent,
+            "Authorization": self.token,
+        }
+
+        response = await self.temp_mail_class.session.get(url, headers=headers)
+        response_json = response.json()
+
+        return DetailedEmail(response_json, self.temp_mail_class, self.token)
+
+    def __repr__(self):
+        return f"PreviewEmail(email_id='{self.email_id}', sender='{self.sender}', subject='{self.subject}', attachments={self.attachments['count']})"
+
+
+class AsyncTempMail:
+    def __init__(self, user_agent=None):
+        self.user_agent = USER_AGENT if not user_agent else user_agent
+
+    async def __aenter__(self):
+        self.session = AsyncSession(impersonate="chrome110", timeout=99999)
+        return self
+
+    async def __aexit__(self, *args):
+        self.session.close()
+
+    async def fetch_new_email_adress(self):
+        url = f"{BASE_API_URL}/mailbox"
+        headers = {"User-Agent": self.user_agent}
+        response = await self.session.post(url, headers=headers)
+        response_json = response.json()
+
+        return response_json
+
+    async def fetch_all_emails(self, token):
+        url = f"{BASE_API_URL}/messages"
+        headers = {"User-Agent": self.user_agent, "Authorization": token}
+        response = await self.session.get(url, headers=headers)
+        response_json = response.json()
+
+        return [PreviewEmail(email, self, token) for email in response_json["messages"]]
