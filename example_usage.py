4546cff358102b507d2ef90201aa7a9000591f2e
Zai <122824602+Zai-Kun@users.noreply.github.com>
2023-10-30 18:32:00 +0500

    Complete example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -9,45 +9,61 @@ fetched_email_ids = []
 def filter_emails(all_emails):
     filtered_emails = []
     for email in all_emails:
-        if email["_id"] not in fetched_email_ids:
-            fetched_email_ids.append(email["_id"])
+        if email.email_id not in fetched_email_ids:
+            fetched_email_ids.append(email.email_id)
             filtered_emails.append(email)
 
     return filtered_emails
 
 
-def print_email(email):
+async def print_email(email):
     print(f"\nFrom: {email.sender}")
     print(f"Subject: {email.subject}")
-    print(f"Attachments: {email.attachments['count']}")
+    print(f"Attachments count: {email.attachments['count']}")
     print(f"\nBody: \n{email.text_body}")
 
-    print(email.attachments)
+    if email.attachments["count"] > 0:
+        print("Attachments content:\n")
+        for file in email.attachments["files"]:
+            print(f"\nFilename: {file.filename}")
+            print(f"File size: {file.size}")
+            print(f"File mimetype: {file.mimetype}")
+            print("File content: ")
+            if file.mimetype == "text/plain":
+                async for chunk in file.download():  # use this to download any attachment
+                    print(chunk.decode())
+                print()
+                continue
+
+            print("Mimetype not 'text/plain'... skipping")
 
 
 async def main():
     async with AsyncTempMail() as temp_mail:
         response_json = await temp_mail.fetch_new_email_adress()
-        email_adress, token = response_json["mailbox"], response_json["token"]
+
+        email_adress = response_json["mailbox"]
+        token = response_json[
+            "token"  # use this token later to access emails associated with the email address
+        ]
 
         print(f"Temp email adress: {email_adress}\nTemp email adress token: {token}\n")
+        while True:
+            input("Press Enter to check for new emails: ")
+            filtered_emails = filter_emails(
+                await temp_mail.fetch_all_emails(token)
+            )  # filter emails to avoid displaying the same ones next time
 
-        # async for chunch in email.attachments["files"][0].download():
-        #     print(chunch.decode())
+            if len(filtered_emails) > 0:
+                print(f"You have {len(filtered_emails)} new email(s)!")
+                for email in filtered_emails:
+                    detailed_email = (
+                        await email.fetch_detailed_email()
+                    )  # you need to use the 'fetch_detailed_email' function to retrieve the details of the email
+                    await print_email(detailed_email)
+                continue
 
-        # while True:
-        #     input("Press Enter to check for new emails: ")
-        #     filtered_emails = filter_emails(await temp_mail.fetch_all_emails(token))
-
-        #     if len(filtered_emails) > 0:
-        #         print(f"You have {len(filtered_emails)} new email(s)!")
-        #         for email in filtered_emails:
-        #             complete_email = await temp_mail.fetch_email(
-        #                 token, email["_id"]
-        #             )  # you need to use the 'fetch_email' function to retrieve all details of the email
-        #             print_email(complete_email)
-        #     else:
-        #         print("You do not have any new emails. :(\n")
+            print("You do not have any new emails. :(\n")
 
 
 if __name__ == "__main__":
